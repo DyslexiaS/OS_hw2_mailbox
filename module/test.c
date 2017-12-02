553a1ee114751b7a858def318bc1b0586c16ce21
E44046092 <e44046092@ncku.edu.tw>
2017-12-02 20:39:04 +0800

    test

--- module/test.c
+++ module/test.c
@@ -3,8 +3,7 @@
 int compare(char*find, char*str)
 {
 	int len = strlen(find);
-	for(int i=0; i<len; ++i)
-	{
+	for(int i=0; i<len; ++i) {
 		find[i] |= 1<<5;
 		str[i] |= 1<<5;
 	}
@@ -12,14 +11,14 @@ int compare(char*find, char*str)
 	else	return 0;
 }
 
-int main (){
+int main ()
+{
 	char a[] = "hello";
 	char b[] = "input.txt";
 	char str[1000]="";
 	long long int count = 0;
 	FILE* pf = fopen(b,"r");
-	while(fscanf(pf, "%s", str)!=EOF)
-	{
+	while(fscanf(pf, "%s", str)!=EOF) {
 		count += compare(a,str);
 	}
 	printf("%lld\n",count);
